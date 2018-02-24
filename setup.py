0a2b34cf2bc89f7faecbb83b646b6215f5343da9
Abdul <akm.mail@gmail.com>
2018-02-24 11:43:02 -0800

    - Switch from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
 import sys
+from setuptools import setup, find_packages
 sys.path.insert(0, 'AWSIoTPythonSDK')
 import AWSIoTPythonSDK
 currentVersion = AWSIoTPythonSDK.__version__
 
-from distutils.core import setup
+
 setup(
-    name = 'AWSIoTPythonSDK',
-    packages=['AWSIoTPythonSDK', 'AWSIoTPythonSDK.core',
-              'AWSIoTPythonSDK.core.util', 'AWSIoTPythonSDK.core.shadow', 'AWSIoTPythonSDK.core.protocol',
-              'AWSIoTPythonSDK.core.protocol.paho', 'AWSIoTPythonSDK.core.protocol.internal',
-              'AWSIoTPythonSDK.core.protocol.connection', 'AWSIoTPythonSDK.core.greengrass',
-              'AWSIoTPythonSDK.core.greengrass.discovery', 'AWSIoTPythonSDK.exception'],
-    version = currentVersion,
-    description = 'SDK for connecting to AWS IoT using Python.',
-    author = 'Amazon Web Service',
-    author_email = '',
-    url = 'https://github.com/aws/aws-iot-device-sdk-python.git',
-    download_url = 'https://s3.amazonaws.com/aws-iot-device-sdk-python/aws-iot-device-sdk-python-latest.zip',
-    keywords = ['aws', 'iot', 'mqtt'],
-    classifiers = [
+    name='AWSIoTPythonSDK',
+    packages=find_packages(exclude=('tests', )),
+    version=currentVersion,
+    description='SDK for connecting to AWS IoT using Python.',
+    author='Amazon Web Service',
+    author_email='',
+    url='https://github.com/aws/aws-iot-device-sdk-python.git',
+    download_url='https://s3.amazonaws.com/aws-iot-device-sdk-python/aws-iot-device-sdk-python-latest.zip',
+    keywords=['aws', 'iot', 'mqtt'],
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Natural Language :: English",
