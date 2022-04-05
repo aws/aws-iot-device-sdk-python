def51baa65184b6b035dedc692334a0a7a1074b0
Vera Xia <zhvxia@amazon.com>
2022-04-05 14:21:29 -0700

    update run scripts and ci to use AWS keys

--- test-integration/run/run.sh
+++ test-integration/run/run.sh
@@ -32,96 +32,92 @@
 # Scale number must also be specified (see usage)
 
 # Define const
-USAGE="usage: run.sh <whichSDK> <testMode> <NumberOfMQTTMessages> <LengthOfShadowRandomString> <NumberOfNetworkFailure>"
-OdinSetForMutualAuth_Desktop="com.amazon.aws.iot.sdk.desktop.certificate"
-OdinSetForWebsocket_Desktop="com.amazon.aws.iot.sdk.desktop.websockets"
-OdinSetForMutualAuth_TodWorker="com.amazonaws.iot.device.sdk.credentials.testing.certificates.us-east-1"
+USAGE="usage: run.sh <testMode> <NumberOfMQTTMessages> <LengthOfShadowRandomString> <NumberOfNetworkFailure>"
+
 AWSMutualAuth_TodWorker_private_key="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestPrivateKey-vNUQU8"
 AWSMutualAuth_TodWorker_certificate="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestCertificate-vTRwjE"
+AWSMutualAuth_Desktop_private_key="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestDesktopPrivateKey-DdC7nv"
+AWSMutualAuth_Desktop_certificate="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestDesktopCertificate-IA4xbj"
 
-OdinSetForGGDiscovery_TodWorker="com.amazonaws.iot.device.sdk.credentials.testing.gg.discovery.us-east-1"
-OdinSetForWebsocket_TodWorker="com.amazonaws.iot.device.sdk.credentials.testing.websocket"
-RetrieveOdinMaterial="./test-integration/Tools/retrieve-material.sh"
+AWSGGDiscovery_TodWorker_private_key="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestGGDiscoveryPrivateKey-YHQI1F"
+AWSGGDiscovery_TodWorker_certificate="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestGGDiscoveryCertificate-TwlAcS"
+
+AWSSecretForWebsocket_TodWorker_KeyId="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestWebsocketAccessKeyId-1YdB9z"
+AWSSecretForWebsocket_TodWorker_SecretKey="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestWebsocketSecretAccessKey-MKTSaV"
+AWSSecretForWebsocket_Desktop_KeyId="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestWebsocketAccessKeyId-1YdB9z"
+AWSSecretForWebsocket_Desktop_SecretKey="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestWebsocketSecretAccessKey-MKTSaV"
+
 RetrieveAWSKeys="./test-integration/Tools/retrieve-key.py"
 CREDENTIAL_DIR="./test-integration/Credentials/"
 TEST_DIR="./test-integration/IntegrationTests/"
 CA_CERT_URL="https://www.amazontrust.com/repository/AmazonRootCA1.pem"
 CA_CERT_PATH=${CREDENTIAL_DIR}rootCA.crt
-ACCESS_KEY_ID_ARN="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestWebsocketAccessKeyId-1YdB9z"
-ACCESS_KEY_ARN="arn:aws:secretsmanager:us-east-1:123124136734:secret:V1IotSdkIntegrationTestWebsocketSecretAccessKey-MKTSaV"
+
 
 
 
 # If input args not correct, echo usage
-if [ $# -ne 5 ]; then
+if [ $# -ne 4 ]; then
     echo ${USAGE}
 else
 # Description
     echo "[STEP] Start run.sh"
     echo "***************************************************"
-    echo "About to start integration tests for $1..."
-    echo "Test Mode: $2"
+    echo "About to start integration tests for IoTPySDK..."
+    echo "Test Mode: $1"
 # Determine the Python versions need to test for this SDK
     pythonExecutableArray=()
-    if [ "$1"x == "IoTYunSDK"x ]; then
-        pythonExecutableArray[0]="2"
-    elif [ "$1"x == "IoTPySDK"x ]; then
-        pythonExecutableArray[0]="3"
-    else
-        echo "Cannot determine Python executables for this unrecognized SDK!"
-        exit 1
-    fi
+    pythonExecutableArray[0]="3"
 # Retrieve credentials as needed from Odin
     TestMode=""
     echo "[STEP] Retrieve credentials from Odin"
     echo "***************************************************"
-    if [ "$2"x == "MutualAuth"x -o "$2"x == "MutualAuthT"x ]; then
-    	OdinSetName=${OdinSetForMutualAuth_TodWorker}
+    if [ "$1"x == "MutualAuth"x -o "$1"x == "MutualAuthT"x ]; then
         AWSSetName_privatekey=${AWSMutualAuth_TodWorker_private_key}
     	AWSSetName_certificate=${AWSMutualAuth_TodWorker_certificate}
-    	OdinSetDRSName=${OdinSetForGGDiscovery_TodWorker}
+    	AWSDRSName_privatekey=${AWSGGDiscovery_TodWorker_private_key}
+        AWSDRSName_certificate=${AWSGGDiscovery_TodWorker_certificate}
         TestMode="MutualAuth"
-        if [ "$2"x == "MutualAuthT"x ]; then
-            OdinSetName=${OdinSetForMutualAuth_Desktop}
+        if [ "$1"x == "MutualAuthT"x ]; then
+            AWSSetName_privatekey=${AWSMutualAuth_Desktop_private_key}
+    	    AWSSetName_certificate=${AWSMutualAuth_Desktop_certificate}
         fi
-    	OdinMaterialTypeCertificate="Certificate"
-    	OdinMaterialTypeKey="PrivateKey"
     	python ${RetrieveAWSKeys} ${AWSSetName_certificate} > ${CREDENTIAL_DIR}certificate.pem.crt
     	python ${RetrieveAWSKeys} ${AWSSetName_privatekey} > ${CREDENTIAL_DIR}privateKey.pem.key
         curl -s "${CA_CERT_URL}" > ${CA_CERT_PATH}
         echo -e "URL retrieved certificate data:\n$(cat ${CA_CERT_PATH})\n"
-    	#${RetrieveOdinMaterial} ${OdinSetDRSName} ${OdinMaterialTypeCertificate} > ${CREDENTIAL_DIR}certificate_drs.pem.crt
-    	#${RetrieveOdinMaterial} ${OdinSetDRSName} ${OdinMaterialTypeKey} > ${CREDENTIAL_DIR}privateKey_drs.pem.key
-    elif [ "$2"x == "Websocket"x -o "$2"x == "WebsocketT"x ]; then
-    	OdinSetName=${OdinSetForWebsocket_TodWorker}
+    	python ${RetrieveAWSKeys} ${AWSDRSName_certificate} > ${CREDENTIAL_DIR}certificate_drs.pem.crt
+    	python ${RetrieveAWSKeys} ${AWSDRSName_privatekey} > ${CREDENTIAL_DIR}privateKey_drs.pem.key
+    elif [ "$1"x == "Websocket"x -o "$1"x == "WebsocketT"x ]; then
+    	ACCESS_KEY_ID_ARN=$(python ${RetrieveAWSKeys} ${AWSSecretForWebsocket_TodWorker_KeyId})
+        ACCESS_SECRET_KEY_ARN=$(python ${RetrieveAWSKeys} ${AWSSecretForWebsocket_TodWorker_SecretKey})
         TestMode="Websocket"
-        if [ "$2"x == "WebsocketT"x ]; then
-            OdinSetName=${OdinSetForWebsocket_Desktop}
+        if [ "$1"x == "WebsocketT"x ]; then
+            ACCESS_KEY_ID_ARN=$(python ${RetrieveAWSKeys} ${AWSSecretForWebsocket_Desktop_KeyId})
+            ACCESS_SECRET_KEY_ARN=$(python ${RetrieveAWSKeys} ${AWSSecretForWebsocket_Desktop_SecretKey})
         fi
-    	OdinMaterialTypeID="Principal"
-    	OdinMaterialTypeSecret="Credential"
-        python ${RetrieveAWSKeys} ${ACCESS_KEY_ID_ARN} > KEY_ID
-        python ${RetrieveAWSKeys} ${ACCESS_KEY_ARN} > SECRET_KEY
-    	#SECRET_KEY=$(${RetrieveOdinMaterial} ${OdinSetName} ${OdinMaterialTypeSecret})
-        export AWS_ACCESS_KEY_ID=${KEY_ID}
-        export AWS_SECRET_ACCESS_KEY=${SECRET_KEY}
+        echo ${ACCESS_KEY_ID_ARN}
+        echo ${ACCESS_SECRET_KEY_ARN}
+        export AWS_ACCESS_KEY_ID=${ACCESS_KEY_ID_ARN}
+        export AWS_SECRET_ACCESS_KEY=${ACCESS_SECRET_KEY_ARN}
         curl -s "${CA_CERT_URL}" > ${CA_CERT_PATH}
         echo -e "URL retrieved certificate data:\n$(cat ${CA_CERT_PATH})\n"
-    elif [ "$2"x == "ALPN"x -o "$2"x == "ALPNT"x ]; then
-        OdinSetName=${OdinSetForMutualAuth_TodWorker}
-    	OdinSetDRSName=${OdinSetForGGDiscovery_TodWorker}
+    elif [ "$1"x == "ALPN"x -o "$1"x == "ALPNT"x ]; then
+        AWSSetName_privatekey=${AWSMutualAuth_TodWorker_private_key}
+    	AWSSetName_certificate=${AWSMutualAuth_TodWorker_certificate}
+    	AWSDRSName_privatekey=${AWSGGDiscovery_TodWorker_private_key}
+        AWSDRSName_certificate=${AWSGGDiscovery_TodWorker_certificate}
         TestMode="ALPN"
-        if [ "$2"x == "ALPNT"x ]; then
-            OdinSetName=${OdinSetForMutualAuth_Desktop}
+        if [ "$1"x == "ALPNT"x ]; then
+            AWSSetName_privatekey=${AWSMutualAuth_Desktop_private_key}
+    	    AWSSetName_certificate=${AWSMutualAuth_Desktop_certificate}
         fi
-        OdinMaterialTypeCertificate="Certificate"
-    	OdinMaterialTypeKey="PrivateKey"
-    	${RetrieveOdinMaterial} ${OdinSetName} ${OdinMaterialTypeCertificate} > ${CREDENTIAL_DIR}certificate.pem.crt
-    	${RetrieveOdinMaterial} ${OdinSetName} ${OdinMaterialTypeKey} > ${CREDENTIAL_DIR}privateKey.pem.key
+        python ${RetrieveAWSKeys} ${AWSSetName_certificate} > ${CREDENTIAL_DIR}certificate.pem.crt
+    	python ${RetrieveAWSKeys} ${AWSSetName_privatekey} > ${CREDENTIAL_DIR}privateKey.pem.key
         curl -s "${CA_CERT_URL}" > ${CA_CERT_PATH}
         echo -e "URL retrieved certificate data:\n$(cat ${CA_CERT_PATH})\n"
-    	#${RetrieveOdinMaterial} ${OdinSetDRSName} ${OdinMaterialTypeCertificate} > ${CREDENTIAL_DIR}certificate_drs.pem.crt
-    	#${RetrieveOdinMaterial} ${OdinSetDRSName} ${OdinMaterialTypeKey} > ${CREDENTIAL_DIR}privateKey_drs.pem.key
+    	python ${RetrieveAWSKeys} ${AWSDRSName_certificate} > ${CREDENTIAL_DIR}certificate_drs.pem.crt
+    	python ${RetrieveAWSKeys} ${AWSDRSName_privatekey} > ${CREDENTIAL_DIR}privateKey_drs.pem.key
     else
     	echo "Mode not supported"
     	exit 1
@@ -135,57 +131,48 @@ else
     	echo "Cannot find SDK ZIP package"
     	exit 2
     fi
-    if [ "$1"x ==  "IoTYunSDK"x ]; then
-        unzip -q ${ZIPLocation} -d ./test-integration/IntegrationTests/TestToolLibrary/SDKPackage/
-    elif [ "$1"x == "IoTPySDK"x ]; then
-        cp -R ${ZIPLocation} ./test-integration/IntegrationTests/TestToolLibrary/SDKPackage/
-    else
-        echo "Error in getSDKZIP"
-        exit 2
-    fi
+    cp -R ${ZIPLocation} ./test-integration/IntegrationTests/TestToolLibrary/SDKPackage/
 # Obtain Python executable
-    for iii in "${pythonExecutableArray[@]}"
+
+    echo "***************************************************"
+    for file in `ls ${TEST_DIR}`
     do
-        echo "***************************************************"
-        for file in `ls ${TEST_DIR}`
-        do
-            # if [ ${file}x == "IntegrationTestMQTTConnection.py"x ]; then
-            if [ ${file##*.}x == "py"x ]; then
-                echo "[SUB] Running test: ${file}..."
+        # if [ ${file}x == "IntegrationTestMQTTConnection.py"x ]; then
+        if [ ${file##*.}x == "py"x ]; then
+            echo "[SUB] Running test: ${file}..."
 
-                Scale=10
-                case "$file" in
-                    "IntegrationTestMQTTConnection.py") Scale=$3
-                    ;;
-                    "IntegrationTestShadow.py") Scale=$4
-                    ;;
-                    "IntegrationTestAutoReconnectResubscribe.py") Scale=""
-                    ;;
-                    "IntegrationTestProgressiveBackoff.py") Scale=$5
-                    ;;
-                    "IntegrationTestConfigurablePublishMessageQueueing.py") Scale=""
-                    ;;
-                    "IntegrationTestDiscovery.py") Scale=""
-                    ;;
-                    "IntegrationTestAsyncAPIGeneralNotificationCallbacks.py") Scale=""
-                    ;;
-                    "IntegrationTestOfflineQueueingForSubscribeUnsubscribe.py") Scale=""
-                    ;;
-                    "IntegrationTestClientReusability.py") Scale=""
-                    ;;
-                    "IntegrationTestJobsClient.py") Scale=""
-                esac
+            Scale=10
+            case "$file" in
+                "IntegrationTestMQTTConnection.py") Scale=$2
+                ;;
+                "IntegrationTestShadow.py") Scale=$3
+                ;;
+                "IntegrationTestAutoReconnectResubscribe.py") Scale=""
+                ;;
+                "IntegrationTestProgressiveBackoff.py") Scale=$4
+                ;;
+                "IntegrationTestConfigurablePublishMessageQueueing.py") Scale=""
+                ;;
+                "IntegrationTestDiscovery.py") Scale=""
+                ;;
+                "IntegrationTestAsyncAPIGeneralNotificationCallbacks.py") Scale=""
+                ;;
+                "IntegrationTestOfflineQueueingForSubscribeUnsubscribe.py") Scale=""
+                ;;
+                "IntegrationTestClientReusability.py") Scale=""
+                ;;
+                "IntegrationTestJobsClient.py") Scale=""
+            esac
 
-                python3 ${TEST_DIR}${file} ${TestMode} ${Scale}
-                currentTestStatus=$?
-                echo "[SUB] Test: ${file} completed. Exiting with status: ${currentTestStatus}"
-                if [ ${currentTestStatus} -ne 0 ]; then
-                    echo "!!!!!!!!!!!!!Test: ${file} in Python version ${iii}.x failed.!!!!!!!!!!!!!"
-                    exit ${currentTestStatus}
-                fi
-                echo ""
+            python ${TEST_DIR}${file} ${TestMode} ${Scale}
+            currentTestStatus=$?
+            echo "[SUB] Test: ${file} completed. Exiting with status: ${currentTestStatus}"
+            if [ ${currentTestStatus} -ne 0 ]; then
+                echo "!!!!!!!!!!!!!Test: ${file} in Python version ${iii}.x failed.!!!!!!!!!!!!!"
+                exit ${currentTestStatus}
             fi
-        done
-        echo "All integration tests passed for Python version ${iii}.x."
+            echo ""
+        fi
     done
+    echo "All integration tests passed"
 fi
