533b588a0a5a806bb9c614305b4bf9fd65e7556f
Vera Xia <zhvxia@amazon.com>
2022-04-12 11:13:16 -0700

    update package name

---
+++ setup_test.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(0, 'AWSIoTPythonSDK')
+import AWSIoTPythonSDK
+currentVersion = AWSIoTPythonSDK.__version__
+
+from distutils.core import setup
+setup(
+    name = 'AWSIoTPythonSDK-V1',
+    packages=['AWSIoTPythonSDK', 'AWSIoTPythonSDK.core',
+              'AWSIoTPythonSDK.core.util', 'AWSIoTPythonSDK.core.shadow', 'AWSIoTPythonSDK.core.protocol',
+              'AWSIoTPythonSDK.core.jobs',
+              'AWSIoTPythonSDK.core.protocol.paho', 'AWSIoTPythonSDK.core.protocol.internal',
+              'AWSIoTPythonSDK.core.protocol.connection', 'AWSIoTPythonSDK.core.greengrass',
+              'AWSIoTPythonSDK.core.greengrass.discovery', 'AWSIoTPythonSDK.exception'],
+    version = currentVersion,
+    description = 'SDK for connecting to AWS IoT using Python.',
+    author = 'Amazon Web Service',
+    author_email = '',
+    url = 'https://github.com/aws/aws-iot-device-sdk-python.git',
+    download_url = 'https://s3.amazonaws.com/aws-iot-device-sdk-python/aws-iot-device-sdk-python-latest.zip',
+    keywords = ['aws', 'iot', 'mqtt'],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
